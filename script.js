de654afc7abc66d321c6c6260e3005a167413a45
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-16 13:23:43 +0000

    add function called setTime

--- script.js
+++ script.js
@@ -44,6 +44,13 @@ toggle.addEventListener("click", (e) => {
 function setTime() {
   const time = new Date();
   const month = time.getMonth();
+  const day = time.getDay();
+  const hours = time.getHours();
+  const hoursForClock = hours % 12;
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+
+  hourEl.style.transform = `translate(-50%, -100%) rotate()`;
 }
 
 setTime();
